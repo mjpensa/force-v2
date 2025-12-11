0221bfa1f83a59e70f9b80027636f87012abed8d
Pensa <matthew.pensa@bip-group.com>
2025-12-11 14:23:28 -0500

    Fix demo slide title layout: 1-2-2-1 word pattern per line

--- Public/components/views/SlidesView.js
+++ Public/components/views/SlidesView.js
@@ -178,7 +178,7 @@ function renderSlide(slide, index) {
 // ========================================
 const DEMO_SLIDE = {
   tagline: 'LOREM IPSUM',
-  title: 'Lorem\nipsum\nsit amet\nsit lorem',
+  title: 'Lorem\nipsum sit\namet sit\nlorem',
   paragraph1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat.',
   paragraph2: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit sed quia consequuntur magni dolores eos ratione voluptatem.'
 };
